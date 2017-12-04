0f66adda311dd476f08aeb642f099295172c444d
pizzaboy75 <34249331+pizzaboy75@users.noreply.github.com>
2017-12-04 23:34:11 +0100

    Typo

--- app/scheduler.py
+++ app/scheduler.py
@@ -80,7 +80,7 @@ class Scheduler(object):
 
         return True
 
-        def clear_all_alarm(self):
+    def clear_all_alarm(self):
         for day in [0, 1, 2, 3, 4, 5, 6] :
             self.cron.remove_all(comment="ON for %s" % day)
             self.cron.remove_all(comment="OFF for %s" % day)
