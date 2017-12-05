8803888e27457b0c58547e7ea0f7800b92013703
pizzaboy75 <34249331+pizzaboy75@users.noreply.github.com>
2017-12-05 03:29:34 +0100

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 # -- Alarm Settings
 PIN = 10                       # Pin to which the PowerSwitch Tail is connected
-ALARM_DURATION = 15            # Light duration in minutes
+ALARM_DURATION = 30            # Light duration in minutes
 
 # -- General Config
 DEBUG = True
